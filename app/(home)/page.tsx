f5d44504113b08a965b395157de0f4cd26acf96c
Shreekar11 <shreekargade2004@gmail.com>
2024-03-30 14:33:07 +0530

    changes completed

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { UserData } from "@/type";
+import { auth } from "@/lib/firebase";
 import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth"
 
 export default function Home() {
+
+  const [userDetails] = useAuthState(auth);
 
   const [user, setUser] = useState<UserData>();
   useEffect(() => {
